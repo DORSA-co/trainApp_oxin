d68a23564334530fa7d4d3e23a2e0784463c7870
Milad Moltaji <m.moltaji5@gmail.com>
2021-12-07 16:40:57 +0330

    delete class tracker

--- main.py
+++ main.py
@@ -14,24 +14,6 @@ from app_settings import Settings
 import cv2
 
 
-class MouseTracker(QtCore.QObject):
-    positionChanged = QtCore.pyqtSignal(QtCore.QPoint)
-
-    def __init__(self, widget):
-        # super().__init__(widget)
-        self._widget = widget
-        self.widget.setMouseTracking(True)
-        self.widget.installEventFilter(self)
-
-    @property
-    def widget(self):
-        return self._widget
-
-    def eventFilter(self, o, e):
-        if o is self.widget and e.type() == QtCore.QEvent.MouseMove:
-            self.positionChanged.emit(e.pos())
-        return super().eventFilter(o, e)
-
 
 ui, _ = loadUiType("oxin.ui")
 os.environ["QT_FONT_DPI"] = "96" # FIX Problem for High DPI and Scale above 100%
