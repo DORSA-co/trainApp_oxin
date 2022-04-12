2582eae8c9472bdccae477a8be62af916e80f84d
ReyhaneAbtahi <79043345+ReyhaneAbtahi@users.noreply.github.com>
2022-04-12 10:03:53 +0430

    Add files via upload

---
+++ Train_modules/deep_utils/callbacks.py
@@ -0,0 +1,13 @@
+from tensorflow import keras
+import os
+
+class CustomCallback(keras.callbacks.Callback):
+    
+    def __init__(self,out_path):
+        #super.__init__(self)
+        self.out_path = out_path
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        self.model.save( self.out_path)
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
