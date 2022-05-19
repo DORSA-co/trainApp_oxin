5048cd69ad07c90cecaae221457bcdde6e268fe0
ReyhaneAbtahi <79043345+ReyhaneAbtahi@users.noreply.github.com>
2022-05-19 11:52:37 +0430

    Update train_api.py

--- train_api.py
+++ train_api.py
@@ -57,9 +57,9 @@ def train_binary(binary_algorithm_name, binary_input_size, binary_input_type, bi
                                                               batch_size=binary_batch,
                                                               validation_split=binary_vs)
     if binary_algorithm_name == ALGORITHM_NAMES['binary'][0]:
-        model = models.xception_cnn(input_size=binary_input_size + (3,), lr=binary_lr, num_class=1, mode=models.BINARY)
+        model = models.xception_cnn(input_size=binary_input_size + (3,), learning_rate=binary_lr, num_class=1, mode=models.BINARY)
     elif binary_algorithm_name == ALGORITHM_NAMES['binary'][1]:
-        model = models.resnet_cnn(input_size=binary_input_size + (3,), lr=binary_lr, num_class=1, mode=models.BINARY)
+        model = models.resnet_cnn(input_size=binary_input_size + (3,), learning_rate=binary_lr, num_class=1, mode=models.BINARY)
 
     my_callback = callbacks.CustomCallback(os.path.join(weights_path, 'checkpoint_bin.h5'), model_type='binary', api_obj=api_obj)
     
@@ -74,11 +74,11 @@ def train_binary(binary_algorithm_name, binary_input_size, binary_input_type, bi
     model.save(os.path.join(weights_path, 'binary_model.h5'))
 
     if binary_algorithm_name == ALGORITHM_NAMES['binary'][0]:
-        model = models.xception_cnn(binary_input_size + (3,), lr=binary_lr, num_class=1, mode=models.CATEGORICAL, fine_tune_layer=120,
+        model = models.xception_cnn(binary_input_size + (3,), learning_rate=binary_lr, num_class=1, mode=models.CATEGORICAL, fine_tune_layer=120,
                                     weights=os.path.join(weights_path, 'checkpoint_bin.h5'))
 
     if binary_algorithm_name == ALGORITHM_NAMES['binary'][1]:
-        model = models.resnet_cnn(binary_input_size + (3,), lr=binary_lr, num_class=1, mode=models.CATEGORICAL, fine_tune_layer=120,
+        model = models.resnet_cnn(binary_input_size + (3,), learning_rate=binary_lr, num_class=1, mode=models.CATEGORICAL, fine_tune_layer=120,
                                     weights=os.path.join(weights_path, 'checkpoint_bin.h5'))
 
     my_callback = callbacks.CustomCallback(os.path.join(weights_path, 'checkpoint.h5'), model_type='binary', api_obj=api_obj)
