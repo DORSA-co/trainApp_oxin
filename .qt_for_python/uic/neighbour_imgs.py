a437dc1d7ee06714132ad566409db89846aaed9f
milad <m.moltaji@yahoo.com>
2022-05-21 14:53:26 +0430

    merge ali

--- .qt_for_python/uic/neighbour_imgs.py
+++ .qt_for_python/uic/neighbour_imgs.py
@@ -3,11 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'neighbour_imgs.ui'
 ##
-<<<<<<< HEAD
 ## Created by: Qt User Interface Compiler version 6.2.4
-=======
-## Created by: Qt User Interface Compiler version 6.2.2
->>>>>>> ali
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
