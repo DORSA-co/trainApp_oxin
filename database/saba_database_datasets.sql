64c052287dba6317e2a97fdc948576cc2b7cc5b1
ReyhaneAbtahi <r.abtahi1378@gmail.com>
2022-05-28 15:05:47 +0430

    datasets & live

--- database/saba_database_datasets.sql
+++ database/saba_database_datasets.sql
@@ -1,53 +1,70 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: saba_database
--- ------------------------------------------------------
--- Server version	8.0.28
+-- Host: localhost:3306
+-- Generation Time: May 28, 2022 at 10:29 AM
+-- Server version: 8.0.29-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `saba_database`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `datasets`
 --
 
-DROP TABLE IF EXISTS `datasets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `datasets` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `name` varchar(45) NOT NULL,
   `user_own` varchar(45) NOT NULL,
-  `path` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `path` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `split_size` varchar(20) NOT NULL DEFAULT '(300, 300)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `datasets`
 --
 
-LOCK TABLES `datasets` WRITE;
-/*!40000 ALTER TABLE `datasets` DISABLE KEYS */;
-INSERT INTO `datasets` VALUES (1,'ali_1','ali','D:\\trainApp_oxin\\dataset'),(2,'sad','sdf','sdsg'),(3,'dfsdf','ali','sdfsdf'),(4,'ghnfgn','ali','ghjghj'),(5,'asd','ali','asdasd'),(6,'asdasd','asda','asdaw'),(7,'asda','ali','casdasd'),(8,'dfg','dfg','dfg'),(9,'dataset_name','ali','adwad'),(10,'1','ali','D:/1'),(11,'1asdad','1','D:/1asdad'),(12,'1asdadasdas','te','D:/1asdadasdas'),(13,'dataset_name','ali','adwad'),(14,'12','1','D:/12');
-/*!40000 ALTER TABLE `datasets` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `datasets` (`id`, `name`, `user_own`, `path`, `split_size`) VALUES
+(0, 'default_dataset', 'Default', 'default_dataset', '(300, 300)'),
+(49, 'MyDS', 'ali', 'MyDS', '(300, 300)');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `datasets`
+--
+ALTER TABLE `datasets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `datasets`
+--
+ALTER TABLE `datasets`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-05-23  9:25:30
